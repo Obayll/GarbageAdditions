b1dd44b7223092cc43be63837de4c17da241b479
Obayll <dajordster@gmail.com>
2023-03-30 20:10:44 -0400

    initial commit

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityModManagerNet;
+using HarmonyLib;
+using BlueprintCore.Utils;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Blueprints.Configurators.Root;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Root;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace WrathMod
+{
+    public static class Main
+    {
+
+    }
+}
